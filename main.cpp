69dab6f30a7a9e566769b76ac8e63cabeb0fa62f
richelbilderbeek <richel@richelbilderbeek.nl>
2018-01-31 21:01:22 +0100

    EOD

--- main.cpp
+++ main.cpp
@@ -1,2 +1,11 @@
-/// hello main function
-int main() {}
+#include "dialog.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+  QApplication a(argc, argv);
+  Dialog w;
+  w.show();
+
+  return a.exec();
+}
