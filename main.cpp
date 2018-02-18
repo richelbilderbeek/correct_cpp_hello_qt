3285abc3d6bade2a5f31b8c6861f01362548abdf
richelbilderbeek <richel@richelbilderbeek.nl>
2018-02-18 15:52:51 +0100

    Correct C++ update

--- main.cpp
+++ main.cpp
@@ -1,12 +1,1 @@
-#include "dialog.h"
-#include <QApplication>
-
-///A 'Hello Qt' program
-int main(int argc, char *argv[])
-{
-  QApplication a(argc, argv);
-  Dialog w;
-  w.show();
-
-  return a.exec();
-}
+main() {}
